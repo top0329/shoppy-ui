b51b567963bc87ae9296e2723f3e34a8a910140c
top0329 <top000329@gmail.com>
2025-03-10 14:12:18 +0200

    feat: get products

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,9 @@ export default async function RootLayout({
         <Providers authenticated={isAuthenticated}>
           <CssBaseline />
           <Header logout={logout} />
-          <Container>{children}</Container>
+          <Container className={isAuthenticated ? "mt-10" : ""}>
+            {children}
+          </Container>
         </Providers>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
-import CreateProductFab from "./products/create-product-fab";
+import CreateProductFab from "./products/create-product/create-product-fab";
+import Products from "./products/products";
 
 export default async function Home() {
-  return <CreateProductFab />;
+  return (
+    <>
+      <Products />
+      <CreateProductFab />
+    </>
+  );
 }
